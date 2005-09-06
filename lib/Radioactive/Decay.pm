1e736e865c8df49688325b182db68e54c2a25ad0
Tony Bowden <tony@tmtm.com>
2005-09-06 18:39:47 +0000

    Import of TMTM/Radioactive-Decay-1.00 from CPAN.
    gitpan-cpan-distribution: Radioactive-Decay
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         TMTM/Radioactive-Decay-1.00.tar.gz
    gitpan-cpan-author:       TMTM
    gitpan-cpan-maturity:     released

--- lib/Radioactive/Decay.pm
+++ lib/Radioactive/Decay.pm
@@ -1,7 +1,5 @@
 package Radioactive::Decay;
 
-=pod
-
 =head1 NAME
 
 Radioactive::Decay - allow scalar values to decay over time
@@ -27,29 +25,39 @@ time.
 For example, if you set a half-life of 30 seconds, then a variable which
 is set to 100 now will be 25 in a minute's time.
 
-We're sure there are all manner of useful applications for this, and hopefully
-someone will let us know what they are.
+We're sure there are all manner of useful applications for this, and
+hopefully someone will let us know what they are.
 
 =head1 AUTHOR
 
-  Marty <marty@kasei.com>, Tony Bowden <tony@tmtm.com>.
-  Comments should be made collectively to <perl-decay@tmtm.com>
+Tony Bowden and Marty Pauley
 
-=head1 LICENSE
+=head1 BUGS and QUERIES
 
-This program may be distributed under the same license as Perl itself.
+Please direct all correspondence regarding this module to:
+  bug-Radioactive-Decay@rt.cpan.org
+
+=head1 COPYRIGHT AND LICENSE
+
+  Copyright (C) 2000-2005 Tony Bowden, Marty Pauley
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License; either version
+  2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
 
+$VERSION = "1.00";
 use strict;
-use vars qw/$VERSION/;
-$VERSION = "0.02";
 
 sub TIESCALAR { bless [0,log(2)/$_[1], time], $_[0]; }
 sub STORE     { $_[0]->[0] = $_[1] }
 sub FETCH     { $_[0]->[0] * exp(-$_[0]->[1] * (time - $_[0]->[2])) }
 sub DESTROY   {}
-
 
 return q/
   make me laugh make me cry enrage me don't try to disengage me
